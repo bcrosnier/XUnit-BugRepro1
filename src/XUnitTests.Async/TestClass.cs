86b1feec852640bebf5f90adc7709bcf9cf5beb0
Benjamin Crosnier <benjamin.crosnier@invenietis.com>
2015-08-10 17:48:35 +0200

    Project commit

---
+++ src/XUnitTests.Async/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTests.Async
+{
+    public class TestClass
+    {
+        [Fact]
+        public async Task AsynchronousFactWithTaskReturn()
+        {
+            await Task.Delay( 100 );
+        }
+
+        [Fact]
+        public async void AsynchronousFactWithVoidReturn()
+        {
+            await Task.Delay( 100 );
+        }
+
+        [Fact]
+        public void SynchronousFact()
+        {
+            Task.Delay( 100 ).Wait();
+        }
+    }
+}
